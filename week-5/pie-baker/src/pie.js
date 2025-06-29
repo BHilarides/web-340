b80fe7aea9b5eb510440a7f97e1be3e37e4bf941
Ben Hilarides <ben.hilarides26@gmail.com>
2025-06-29 17:06:40 -0400

    initial commit for pie-baker

---
+++ week-5/pie-baker/src/pie.js
@@ -0,0 +1,13 @@
+/**
+ * Author: Ben Hilarides
+ * Date: 6.29.25
+ * File Name: pie.js
+ * Description: This module exports a function to bake a pie with specified ingredients.
+ */
+"use strict";
+
+function bakePie(pieType, ingredients) {
+  // Your code here
+}
+
+module.exports = { bakePie };
---
+++ week-5/pie-baker/test/pie.spec.js
@@ -0,0 +1,12 @@
+/**
+ * Author: Ben Hilarides
+ * Date: 6.29.25
+ * File Name: pie.spec.js
+ * Description: This module contains tests for the bakePie function in pie.js.
+ */
+
+"use strict";
+
+const { bakePie } = require("../src/pie");
+
+// Your tests here
